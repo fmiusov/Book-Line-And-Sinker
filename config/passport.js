62f59eea6f2311fde00cece71aa1bc16a4be874a
JaysonD <jaduff20@gmail.com>
2020-04-15 15:29:33 -0400

    all eslint errors resolved, checking in with travis functionality

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,6 @@
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
-
 var db = require("../models");
-
 passport.use(
   new LocalStrategy(
     // Our user will sign in using an email, rather than a "username"
@@ -21,9 +19,8 @@ passport.use(
           return done(null, false, {
             message: "Incorrect email.",
           });
-        }
         // If there is a user with the given email, but the password the user gives us is incorrect
-        else if (!dbUser.validPassword(password)) {
+        } else if (!dbUser.validPassword(password)) {
           return done(null, false, {
             message: "Incorrect password.",
           });
@@ -34,17 +31,14 @@ passport.use(
     }
   )
 );
-
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
 passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
-
 passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
-
 // Exporting our configured passport
 module.exports = passport;
