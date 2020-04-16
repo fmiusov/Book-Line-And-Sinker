13df0275da890d54fe6e0f61831cf36ff00a3a65
Zack Hayden <signbarn@gmail.com>
2020-04-16 12:38:36 -0400

    change html route back

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,17 +12,9 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  // app.get("/", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/index.html"));
-  // });
 
   app.get("/", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/login", function (req, res) {
