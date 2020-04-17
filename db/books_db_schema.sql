964086426ad31c8de3656e3cf3ebd634e042ff08
Zack Hayden <signbarn@gmail.com>
2020-04-17 11:56:47 -0400

    commenting out create table in schema. we only need to create the db

--- db/books_db_schema.sql
+++ db/books_db_schema.sql
@@ -1,20 +1,20 @@
 drop database if exists goodreadsDb;
 create database goodreadsDb;
-use goodreadsDb;
-create table books (
-  bookId int not null,
-  title varchar(255),
-  authors varchar(255),
-  averageRating int,
-  isbn varchar(16),
-  isbn13 varchar(16),
-  languageCode varchar(16),
-  numPages int,
-  ratingsCount int,
-  textReviewsCount int,
-  publicationDate date,
-  publisher varchar(255),
-  primary key(bookId)
-);
+-- use goodreadsDb;
+-- create table books (
+--   bookId int not null,
+--   title varchar(255),
+--   authors varchar(255),
+--   averageRating int,
+--   isbn varchar(16),
+--   isbn13 varchar(16),
+--   languageCode varchar(16),
+--   numPages int,
+--   ratingsCount int,
+--   textReviewsCount int,
+--   publicationDate date,
+--   publisher varchar(255),
+--   primary key(bookId)
+-- );
 
 -- /* uncheck bookId when importing books.csv */ --
