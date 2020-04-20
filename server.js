22bad30b83ba802572b08312629d35b8224b7837
JaysonD <jaduff20@gmail.com>
2020-04-20 07:13:58 -0400

    adding force:true

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({ force: true}).then(function () {
   app.listen(PORT, function () {
     console.log(`Server listening on: http://localhost:${PORT}`);
   });
